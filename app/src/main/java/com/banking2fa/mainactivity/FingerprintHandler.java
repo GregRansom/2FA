f773be75a523eec81c23e50f8939a78eb3bddc69
GregRansom <32838628+GregRansom@users.noreply.github.com>
2019-10-02 08:01:08 +0200

    updated SDK files
    SDK files were out of date and prevented emulation from starting.

--- app/src/main/java/com/banking2fa/mainactivity/FingerprintHandler.java
+++ app/src/main/java/com/banking2fa/mainactivity/FingerprintHandler.java
@@ -31,7 +31,7 @@ public class FingerprintHandler extends FingerprintManager.AuthenticationCallbac
         cancellationSignal = new CancellationSignal();
         if (ActivityCompat.checkSelfPermission(context, Manifest.permission.USE_FINGERPRINT) != PackageManager.PERMISSION_GRANTED) {
             return;
-            
+
         }
         manager.authenticate(cryptoObject, cancellationSignal, 0, this, null);
     }
