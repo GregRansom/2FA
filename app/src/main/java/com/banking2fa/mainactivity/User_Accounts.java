eeb6671dce743194ca739a99daade7ce5d414e93
GregRansom <32838628+GregRansom@users.noreply.github.com>
2019-10-04 20:51:23 +0200

    created event listeners for main UI
    added button even listeners to log out and finish the authentication for the accounts page.

--- app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
+++ app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
@@ -1,14 +1,34 @@
 package com.banking2fa.mainactivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class User_Accounts extends AppCompatActivity {
 
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__accounts);
+    }
+
+    public void cancel_transfer(){
+        Intent logout = new Intent(this.context, FingerprintHandler.class);
+        context.startActivity(logout);
+    }
+
+    public void finish_transfer(){
+        //TODO: handle the finishing of the transfer
+    }
+
+    private boolean request_auth(float amount){
+        if (amount >= 2000){
+            return true;
+        }
+        return false;
     }
 }
