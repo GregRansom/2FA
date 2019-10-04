83f0df0391c844b42f21a14e6e9d6ecd9ce5f6af
GregRansom <32838628+GregRansom@users.noreply.github.com>
2019-10-04 21:22:39 +0200

    added some comments and cleaned up a little bit

--- app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
+++ app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
@@ -16,17 +16,20 @@ public class User_Accounts extends AppCompatActivity {
         setContentView(R.layout.activity_user__accounts);
     }
 
+    //listens for the cancel button to be clicked.
     public void cancel_transfer(android.view.View canceled){
         Intent logout = new Intent(this.context, FingerprintHandler.class);
         context.startActivity(logout);
     }
 
+    //listens for the finish button to be clicked.
+    //then performs the verification check on the amount.
     public void finish_transfer(android.view.View finished){
-        start_finish();
-        //TODO: handle the finishing of the transfer
-
+        start_finish(20000.00);
     }
-    private void start_finish(float amount){
+
+    //performs a verification check on the amount being requested to transfer.
+    private void start_finish(double amount){
         if (amount >= 2000){
             request_auth();
         }
