d19e3f844f9d9d5e7a6f323a6ab80f8d462cb8ab
GregRansom <32838628+GregRansom@users.noreply.github.com>
2019-10-05 20:46:02 +0200

    added basic functionality to user_accounts
    User_account now takes the varibales and passes them locally to the correct format.

--- app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
+++ app/src/main/java/com/banking2fa/mainactivity/User_Accounts.java
@@ -3,6 +3,8 @@ package com.banking2fa.mainactivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,7 +27,41 @@ public class User_Accounts extends AppCompatActivity {
     //listens for the finish button to be clicked.
     //then performs the verification check on the amount.
     public void finish_transfer(android.view.View finished){
-        start_finish(20000.00);
+
+        //gets the value of the Amount Field
+        EditText edtamount = (EditText) findViewById(R.id.edtAmount); //gets the input
+        String AmountString = edtamount.getText().toString(); //converts the EditText into local text
+        double Amount = Double.parseDouble(AmountString);       //passes the string into double format
+
+
+        //gets the value of the reference field
+        EditText edtRef = (EditText) findViewById(R.id.edtRef); //gets the input
+        String ref = edtRef.getText().toString(); //converts the input into local text
+
+        //gets/sets the values of the radio groups.
+        //if From A is selected then To B will auto Select, and vice versa
+        RadioButton radFromA = (RadioButton ) findViewById(R.id.radioFromA); //gets the state of the button
+        RadioButton radFromB = (RadioButton ) findViewById(R.id.radioFromB);//gets the state of the button
+        RadioButton radToA = (RadioButton ) findViewById(R.id.radioToA);//gets the state of the button
+        RadioButton radToB = (RadioButton ) findViewById(R.id.radioToB);//gets the state of the button
+
+        boolean FromA = radFromA.isChecked();   //assigned the state locally
+        boolean FromB = radFromB.isChecked();   //assigned the state locally
+        boolean ToA = radToA.isChecked();   //assigned the state locally
+        boolean ToB = radToB.isChecked();   //assigned the state locally
+
+        //does verification check
+        if (FromA){     //if From Account A is selected
+          radToB.setChecked(true);  //add to Account B
+          radToA.setChecked(false); // and NOT to account A
+        }
+        else if (FromB){   // if From Account B is selected
+            radToA.setChecked(true);    //Add to account A
+            radToB.setChecked(false);   // and NOT to account B
+        }
+
+        
+        start_finish(Amount);
     }
 
     //performs a verification check on the amount being requested to transfer.
